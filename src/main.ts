c283d25d211680061f2a5ea1b5f3e84982a74b65
drifterz13 <mixmyxxe@gmail.com>
2022-11-10 07:32:56 +0700

    render audio

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,68 @@ import * as Transcribe from './libs/transcribe'
 const record = document.querySelector<HTMLButtonElement>('.record')!
 const stopButton = document.querySelector<HTMLButtonElement>('.stop')!
 const transcript = document.querySelector<HTMLElement>('.transcript')!
+const soundClips = document.querySelector<HTMLElement>('.sound-clips')!
 
 function writeTranscript(content: string) {
   transcript.innerText += ` ${content}`
+}
+
+function handleSuccess(stream: MediaStream) {
+  let chunks: Blob[] = []
+
+  const mediaRecorder = new MediaRecorder(stream)
+  mediaRecorder.addEventListener('dataavailable', (e) => {
+    console.log('data: ', e.data)
+    if (e.data.size > 0) {
+      chunks.push(e.data)
+    }
+  })
+
+  mediaRecorder.addEventListener('stop', () => {
+    console.log('record stop')
+
+    const clipContainer = document.createElement('article')
+    const clipLabel = document.createElement('p')
+    const audio = document.createElement('audio')
+    const deleteButton = document.createElement('button')
+
+    clipContainer.classList.add('clip')
+    audio.setAttribute('controls', '')
+    deleteButton.innerHTML = 'Delete'
+    clipLabel.innerHTML = 'audio-01'
+
+    clipContainer.appendChild(audio)
+    clipContainer.appendChild(clipLabel)
+    clipContainer.appendChild(deleteButton)
+    soundClips.appendChild(clipContainer)
+
+    const blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' })
+    chunks = []
+    const audioURL = window.URL.createObjectURL(blob)
+    audio.src = audioURL
+  })
+
+  mediaRecorder.start()
+
+  stopButton.addEventListener('click', () => {
+    console.log('stop')
+    mediaRecorder.stop()
+
+    record.disabled = false
+    Transcribe.stopRecording()
+  })
 }
 
 record.addEventListener('click', () => {
   console.log('recording...')
   Transcribe.startRecording('en-US', writeTranscript)
 
-  record.disabled = true
-})
+  navigator.mediaDevices
+    .getUserMedia({
+      video: false,
+      audio: true,
+    })
+    .then(handleSuccess)
 
-stopButton.addEventListener('click', () => {
-  console.log('stop')
-  Transcribe.stopRecording()
-  record.disabled = false
+  record.disabled = true
 })
